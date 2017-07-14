8bfb2428788e3522902f645adfc0bc628f2b68d0
michal <michal.zurek7@gmail.com>
2017-07-14 15:03:16 +0200

    more state handling

--- initial-example-app/order-services/src/main/scala/com/packt/masteringakka/bookstore/order/SalesOrderProcessor.scala
+++ initial-example-app/order-services/src/main/scala/com/packt/masteringakka/bookstore/order/SalesOrderProcessor.scala
@@ -1,11 +1,17 @@
 package com.packt.masteringakka.bookstore.order
 
+import java.util.Date
+
+import akka.actor.FSM.Failure
 import akka.actor.{ActorIdentity, ActorRef, FSM, Identify}
-import com.packt.masteringakka.bookstore.common.{Empty, FullResult, ServiceResult}
+import com.packt.masteringakka.bookstore.common._
 import com.packt.masteringakka.bookstore.domain.book.{Book, FindBook}
+import com.packt.masteringakka.bookstore.domain.credit.ChargeCreditCard
 import com.packt.masteringakka.bookstore.domain.user.{BookstoreUser, FindUserById}
+import com.packt.masteringakka.bookstore.order.SalesOrderProcessor.{InvalidBookIdError, InvalidUserIdError}
 
 import concurrent.duration._
+import scala.language.postfixOps
 
 /**
   * Created by michalz on 17/05/2017.
@@ -13,6 +19,8 @@ import concurrent.duration._
 class SalesOrderProcessor extends FSM[SalesOrderProcessor.State, SalesOrderProcessor.Data] {
 
   import SalesOrderProcessor._
+
+  def unexpectedFail = Failure(FailureType.Service, ServiceResult.UnexpectedFailure )
 
   //  val dao = new SalesOrderPr
 
@@ -29,7 +37,8 @@ class SalesOrderProcessor extends FSM[SalesOrderProcessor.State, SalesOrderProce
   when(ResolvingDependencies, 5 seconds) {
     transform { // transform is gatekeeper for going to next transition
       case Event(ActorIdentity(identifier: ResolutionIdent.Value, actor@Some(ref)), stateData: UnresolvedDependencies) =>
-        val newData = identifier match {
+        log.info("Resolved dependency {}, {}", identifier, ref)
+        val newData: UnresolvedDependencies = identifier match {
           case ResolutionIdent.Book =>
             stateData.copy(bookMgr = actor)
           case ResolutionIdent.User =>
@@ -41,10 +50,13 @@ class SalesOrderProcessor extends FSM[SalesOrderProcessor.State, SalesOrderProce
     } using {
       //here evaluating current state if ready to go to another
       case FSM.State(state, UnresolvedDependencies(inputs, Some(user), Some(book), Some(credit)), _, _, _) =>
+
+        log.info("Resolved all dependencies, looking up entities")
+
         user ! FindUserById(inputs.request.userId)
         val expectedBooks = inputs.request.lineItems.map(_.bookId).toSet
         expectedBooks.foreach(id => book ! FindBook(id))
-        goto(LookingUpEntities) using (ResolvedDependencies(inputs, expectedBooks, None, Map.empty, book, credit))
+        goto(LookingUpEntities) using ResolvedDependencies(inputs, expectedBooks, None, Map.empty, book, credit)
     }
 
     when(LookingUpEntities, 5 seconds) {
@@ -55,24 +67,40 @@ class SalesOrderProcessor extends FSM[SalesOrderProcessor.State, SalesOrderProce
             case None =>
               stateData.originator ! unexpectedFail
               stop()
+
+            case Some(item) if item.quantity > b.inventoryAmount =>
+              stateData.originator ! Failure(FailureType.Validation, ErrorMessage("order.inventory.notavailable", Some("Inventory for an item on this order is no longer available")))
+              stop
+
+            case _ => stay using stateData.copy(books = stateData.books ++ Map(b.id -> b))
           }
 
-        case Event(bUsermaybe: ServiceResult[BookstoreUser], stateData: ResolvedDependencies) =>
-          val newData = bUsermaybe match {
-            case full: FullResult[BookstoreUser] =>
-              stateData.copy(user = Some(full.value))
-            case empty: Empty => stateData //TODO: short circuit
-          }
-          stay using newData
-        case Event(bookMaybe: ServiceResult[Book], stateData: ResolvedDependencies) =>
-          val newData = bookMaybe match {
-            case full: FullResult[Book] =>
-              stateData.copy(books = stateData.books + (full.value.id -> full.value))
-            case empty: Empty => stateData //TODO: short circuit
-          }
-          stay using newData
+        case Event(FullResult(u: BookstoreUser), stateData: ResolvedDependencies) =>
+          stay using stateData.copy(user = Some(u))
+
+        case Event(EmptyResult, data:ResolvedDependencies) =>
+          val (etype, error) =
+            if (sender().path.name == BookMgrName) ("book", InvalidBookIdError)
+            else ("user", InvalidUserIdError )
+          log.info("Unexpected result type of EmptyResult received looking up a {} entity", etype)
+          data.originator ! Failure(FailureType.Validation, error)
+          stop
+
       } using {
+        case FSM.State(state, ResolvedDependencies(inputs, expectedBooks, Some(u), bookMap, userMgr, creditMgr), _, _, _)
+          if bookMap.keySet == expectedBooks =>
 
+          log.info("Successfully looked up all entities and inventory is available, charging credit card")
+          val lineItems = inputs.request.lineItems.
+            flatMap{item =>
+              bookMap.
+                get(item.bookId).
+                map(b => SalesOrderLineItem(0, 0, b.id, item.quantity, item.quantity * b.cost, new Date(), new Date()))
+            }
+
+          val total = lineItems.map(_.cost).sum
+          creditMgr ! ChargeCreditCard(inputs.request.cardInfo, total)
+          goto(ChargingCard) using LookedUpData(inputs, u, lineItems, total)
       }
     }
 
@@ -134,5 +162,14 @@ object SalesOrderProcessor {
   case object Uninitialized extends Data {
     override def originator: ActorRef = ActorRef.noSender
   }
+
+  val UserManagerName = "user-manager"
+  val CreditHandlerName = "credit-handler"
+  val BookMgrName = "book-manager"
+
+  val InvalidBookIdError = ErrorMessage("order.invalid.bookId", Some("You have supplied an invalid book id"))
+  val InvalidUserIdError = ErrorMessage("order.invalid.userId", Some("You have supplied an invalid user id"))
+  val CreditRejectedError = ErrorMessage("order.credit.rejected", Some("Your credit card has been rejected"))
+  val InventoryNotAvailError = ErrorMessage("order.inventory.notavailable", Some("Inventory for an item on this order is no longer available"))
 
 }
